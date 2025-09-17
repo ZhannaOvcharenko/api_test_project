557473e2fcdb4734a79f246ed994fbcd5c91b1ab
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-09-17 15:28:27 +0300

    change in file test_auth_login

--- tests/test_auth_login.py
+++ tests/test_auth_login.py
@@ -5,7 +5,7 @@ from models.user import LoginRequest, LoginResponse
 
 @allure.story("POST /login — авторизация и получение токена")
 def test_login_success(api):
-    payload = LoginRequest(email="test.brog@reqres.in", password="members").model_dump()
+    payload = LoginRequest(email="eve.holt@reqres.in", password="cityslicka").model_dump()
     resp = api.post(LOGIN, json=payload, expected_status=200)
 
     assert resp.status_code == 200
